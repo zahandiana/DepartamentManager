c43befe4dd82dcd97dbc6b4aa665c145d930acce
Zăhan Diana <81300377+zahandiana@users.noreply.github.com>
2024-08-01 15:28:52 +0300

    Add files via upload

---
+++ DepartmentManagerApplication.java
@@ -0,0 +1,14 @@
+package utcn.departmentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utcn.departmentManager.Department.Department;
+
+@SpringBootApplication
+public class DepartmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentManagerApplication.class, args);
+	}
+
+}
